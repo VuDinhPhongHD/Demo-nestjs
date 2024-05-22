38af2ed8c28ac18da61729e350833655227ef931
VuDinhPhong <phong.vudinh@hdwebsoft.dev>
2024-05-22 10:21:22 +0700

    Update filter, sort, search, paging

--- src/pets/pets.resolver.ts
+++ src/pets/pets.resolver.ts
@@ -12,23 +12,32 @@ export class PetsResolver {
     return this.petsService.create(createPetInput);
   }
 
-  @Query(() => [Pet], { name: 'findPetBySpecies' })
-  async findOneBySpecies(@Args('species') species: string): Promise<Pet[]> {
-    const pets = await this.petsService.findOneBySpecies(species);
-    return pets
-  }
+  // @Query(() => [Pet], { name: 'findPetBySpecies' })
+  // async findOneBySpecies(@Args('species') species: string): Promise<Pet[]> {
+  //   const pets = await this.petsService.findOneBySpecies(species);
+  //   return pets
+  // }
 
-  @Query(() => [Pet], { name: "filterPets" })
-  async filterPetsByAgeAndSpecies(
-    @Args('age', { nullable: true }) age: number,
-    @Args('species', { nullable: true }) species: string
-  ): Promise<Pet[]> {
-    const pets = await this.petsService.filterPetsByAgeAndSpecies(age, species);
-    return pets
-  }
+  // @Query(() => [Pet], { name: "filterPets" })
+  // async filterPetsByAgeAndSpecies(
+  //   @Args('age', { nullable: true }) age: number,
+  //   @Args('species', { nullable: true }) species: string
+  // ): Promise<Pet[]> {
+  //   const pets = await this.petsService.filterPetsByAgeAndSpecies(age, species);
+  //   return pets
+  // }
 
+  // Phương thức findAll với các tham số sắp xếp
   @Query(() => [Pet], { name: 'pets' })
-  async findAll(): Promise<Pet[]> {
-    return this.petsService.findAll();
+  async findAll(
+    @Args('sortBy', { nullable: true }) sortBy: string,
+    @Args('sortOrder', { nullable: true }) sortOrder: 'ASC' | 'DESC',
+    @Args('search', { nullable: true }) search: string,
+    @Args('filterAge', { nullable: true }) filterAge: number,
+    @Args('filterSpecies', { nullable: true }) filterSpecies: string,
+    @Args('skip', { nullable: true }) skip?: number,
+    @Args('take', { nullable: true }) take?: number,
+  ): Promise<Pet[]> {
+    return this.petsService.findAll(sortBy, sortOrder, search, filterAge, filterSpecies, skip, take);
   }
 }
--- src/pets/pets.service.ts
+++ src/pets/pets.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Like, Repository } from 'typeorm';
+import { FindManyOptions, Like, Repository } from 'typeorm';
 import { Pet } from './entities/pet.entity';
 import { CreatePetInput } from './dto/create-pet.input';
 
@@ -19,32 +19,65 @@ export class PetsService {
     return this.petsRepository.save(pet);
   }
 
-  async findOneBySpecies(species: string): Promise<Pet[]> {
-    const pets = await this.petsRepository.find({
-      where: { species: Like(`%${species}%`) }
-    });
-    return pets;
+  // async findOneBySpecies(species: string): Promise<Pet[]> {
+  //   const pets = await this.petsRepository.find({
+  //     where: { species: Like(`%${species}%`) }
+  //   });
+  //   return pets;
+  // }
+
+  // async filterPetsByAgeAndSpecies(age: number, species: string): Promise<Pet[]> {
+  //   const filter: any = { age, species };
+  //   if (age) {
+  //     filter.age = age;
+  //   }
+  //   if (species) {
+  //     filter.species = species;
+  //   }
+  //   const queryBuilder = this.petsRepository.createQueryBuilder('pet');
+  //   if (filter.age) {
+  //     queryBuilder.andWhere('pet.age = :age', { age: filter.age });
+  //   }
+  //   if (filter.species) {
+  //     queryBuilder.andWhere('pet.species = :species', { species: filter.species });
+  //   }
+  //   return queryBuilder.getMany();
+  // }
+  async findAll(
+    sortBy?: string,
+    sortOrder?: 'ASC' | 'DESC',
+    search?: string,
+    filterAge?: number,
+    filterSpecies?: string,
+    skip?: number,
+    take?: number,
+  ): Promise<Pet[]> {
+    let options: FindManyOptions<Pet> = {};
+
+    if (sortBy && sortOrder) {
+      options.order = { [sortBy]: sortOrder };
+    }
+
+    if (search) {
+      options.where = { name: Like(`%${search}%`) };
+    }
+
+    if (filterAge) {
+      options.where = { ...options.where, age: filterAge };
+    }
+
+    if (filterSpecies) {
+      options.where = { ...options.where, species: Like(`%${filterSpecies}%`) };
+    }
+    if (skip) {
+      options.skip = skip;
+    }
+
+    if (take) {
+      options.take = take;
+    }
+    return this.petsRepository.find(options);
   }
 
-  async filterPetsByAgeAndSpecies(age: number, species: string): Promise<Pet[]> {
-    const filter: any = { age, species };
-    if (age) {
-      filter.age = age;
-    }
-    if (species) {
-      filter.species = species;
-    }
-    const queryBuilder = this.petsRepository.createQueryBuilder('pet');
-    if (filter.age) {
-      queryBuilder.andWhere('pet.age = :age', { age: filter.age });
-    }
-    if (filter.species) {
-      queryBuilder.andWhere('pet.species = :species', { species: filter.species });
-    }
-    return queryBuilder.getMany();
-  }
 
-  async findAll(): Promise<Pet[]> {
-    return this.petsRepository.find();
-  }
 }
